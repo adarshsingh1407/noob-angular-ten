a320a880cd727c28dbfce553f1e1773ab14705da
Adarsh Singh <adarshsingh1407@gmail.com>
2021-03-09 15:44:01 +0530

    Warning and Error Alert Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ErrorAlertComponent } from './error-alert/error-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
