d00dea7044473444ad70c039204d922f1d765ea6
Adarsh Singh <adarshsingh1407@gmail.com>
2021-03-10 00:56:02 +0530

    bind data to directive

--- src/app/directives/highlighted-text.directive.ts
+++ src/app/directives/highlighted-text.directive.ts
@@ -1,11 +1,17 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightedText]'
 })
-export class HighlightedTextDirective {
+export class HighlightedTextDirective implements OnInit {
+  @Input() defaultColor = 'transparent';
+  @Input() bgColor = 'blue';
 
   @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+  }
 
   @HostListener('mouseenter') onMouseEnter(): void {
     this.backgroundColor = 'blue';
